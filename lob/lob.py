16b71873b207e07494b85f58ad071fccb238b9e8
Kautenja <kautencreations@gmail.com>
2019-12-17 03:58:50 -0600

    comment

--- lob/lob.py
+++ lob/lob.py
@@ -10,60 +10,107 @@ try:  # load the library from the shared object file
 except IndexError:
     raise OSError('missing static lib_lob*.so library!')
 
-Size = ctypes.c_uint32
-Price = ctypes.c_uint64
-Volume = ctypes.c_uint32
+
+class Types:
+    """Types used in the C++ code."""
+    # a type for pointers
+    Pointer = ctypes.c_void_p
+    # a type for the side of the order (i.e., sell or buy)
+    Side = ctypes.c_bool
+    # a type for the unique order ID
+    UID = ctypes.c_uint64
+    # a type for the size of an order
+    Size = ctypes.c_uint32
+    # a type for the price of an order
+    Price = ctypes.c_uint64
+    # a type for the volume measures in the book
+    Volume = ctypes.c_uint32
+
 
 # setup the argument and return types for Initialize
 LIB.new_.argtypes = None
-LIB.new_.restype = ctypes.c_void_p
+LIB.new_.restype = Types.Pointer
 # setup the argument and return types for Delete
-LIB.delete_.argtypes = [ctypes.c_void_p]
+LIB.delete_.argtypes = [Types.Pointer]
 LIB.delete_.restype = None
 
 
+# setup the argument and return types for limit_sell
+LIB.limit_sell.argtypes = [Types.Pointer, Types.UID, Types.Size, Types.Price]
+LIB.limit_sell.restype = None
+# setup the argument and return types for limit_buy
+LIB.limit_buy.argtypes = [Types.Pointer, Types.UID, Types.Size, Types.Price]
+LIB.limit_buy.restype = None
+# setup the argument and return types for limit
+LIB.limit.argtypes = [Types.Pointer, Types.Side, Types.UID, Types.Size, Types.Price]
+LIB.limit.restype = None
+
+
+# setup the argument and return types for has
+LIB.has.argtypes = [Types.Pointer, Types.UID]
+LIB.has.restype = ctypes.c_bool
+# setup the argument and return types for get
+LIB.get.argtypes = [Types.Pointer, Types.UID]
+LIB.get.restype = Types.Pointer
+# setup the argument and return types for cancel
+LIB.cancel.argtypes = [Types.Pointer, Types.UID]
+LIB.cancel.restype = None
+
+
+# setup the argument and return types for market_sell
+LIB.market_sell.argtypes = [Types.Pointer, Types.UID, Types.Size]
+LIB.market_sell.restype = None
+# setup the argument and return types for market_buy
+LIB.market_buy.argtypes = [Types.Pointer, Types.UID, Types.Size]
+LIB.market_buy.restype = None
+# setup the argument and return types for market
+LIB.market.argtypes = [Types.Pointer, Types.Side, Types.UID, Types.Size]
+LIB.market.restype = None
+
 
 # setup the argument and return types for best_sell
-LIB.best_sell.argtypes = [ctypes.c_void_p]
-LIB.best_sell.restype = Price
+LIB.best_sell.argtypes = [Types.Pointer]
+LIB.best_sell.restype = Types.Price
 # setup the argument and return types for best_buy
-LIB.best_buy.argtypes = [ctypes.c_void_p]
-LIB.best_buy.restype = Price
+LIB.best_buy.argtypes = [Types.Pointer]
+LIB.best_buy.restype = Types.Price
 # setup the argument and return types for best
-LIB.best.argtypes = [ctypes.c_void_p, ctypes.c_bool]
-LIB.best.restype = Price
+LIB.best.argtypes = [Types.Pointer, Types.Side]
+LIB.best.restype = Types.Price
+
 
 # setup the argument and return types for volume_sell_price
-LIB.volume_sell_price.argtypes = [ctypes.c_void_p, Price]
-LIB.volume_sell_price.restype = Volume
+LIB.volume_sell_price.argtypes = [Types.Pointer, Types.Price]
+LIB.volume_sell_price.restype = Types.Volume
 # setup the argument and return types for volume_sell
-LIB.volume_sell.argtypes = [ctypes.c_void_p]
-LIB.volume_sell.restype = Volume
+LIB.volume_sell.argtypes = [Types.Pointer]
+LIB.volume_sell.restype = Types.Volume
 # setup the argument and return types for volume_buy_price
-LIB.volume_buy_price.argtypes = [ctypes.c_void_p, Price]
-LIB.volume_buy_price.restype = Volume
+LIB.volume_buy_price.argtypes = [Types.Pointer, Types.Price]
+LIB.volume_buy_price.restype = Types.Volume
 # setup the argument and return types for volume_buy
-LIB.volume_buy.argtypes = [ctypes.c_void_p]
-LIB.volume_buy.restype = Volume
+LIB.volume_buy.argtypes = [Types.Pointer]
+LIB.volume_buy.restype = Types.Volume
 # setup the argument and return types for volume_price
-LIB.volume_price.argtypes = [ctypes.c_void_p, Price]
-LIB.volume_price.restype = Volume
+LIB.volume_price.argtypes = [Types.Pointer, Types.Price]
+LIB.volume_price.restype = Types.Volume
 # setup the argument and return types for volume
-LIB.volume.argtypes = [ctypes.c_void_p]
-LIB.volume.restype = Volume
+LIB.volume.argtypes = [Types.Pointer]
+LIB.volume.restype = Types.Volume
+
 
 # setup the argument and return types for size_at
-LIB.size_at.argtypes = [ctypes.c_void_p, Price]
-LIB.size_at.restype = Size
+LIB.size_at.argtypes = [Types.Pointer, Types.Price]
+LIB.size_at.restype = Types.Size
 # setup the argument and return types for size_sell
-LIB.size_sell.argtypes = [ctypes.c_void_p]
-LIB.size_sell.restype = Size
+LIB.size_sell.argtypes = [Types.Pointer]
+LIB.size_sell.restype = Types.Size
 # setup the argument and return types for size_buy
-LIB.size_buy.argtypes = [ctypes.c_void_p]
-LIB.size_buy.restype = Size
+LIB.size_buy.argtypes = [Types.Pointer]
+LIB.size_buy.restype = Types.Size
 # setup the argument and return types for size
-LIB.size.argtypes = [ctypes.c_void_p]
-LIB.size.restype = Size
+LIB.size.argtypes = [Types.Pointer]
+LIB.size.restype = Types.Size
 
 
 class LimitOrderBook:
@@ -77,41 +124,134 @@ class LimitOrderBook:
         """Delete this limit order book."""
         LIB.delete_(self._book)
 
-    def limit_sell(self, size, price, arrival):
+    def limit_sell(self, order_id, size, price):
         """
-        """
+        Place a sell limit order with given size and price.
 
-    def limit_buy(self, size, price, arrival):
-        """
-        """
+        Args:
+            order_id: the ID for the order to add
+            size: the size of the limit order to place
+            price: the price of the limit order to place
 
-    def limit(self, side, size, price, arrival):
+        Returns:
+            None
+
         """
+        LIB.limit_sell(self._book, order_id, size, price)
+
+    def limit_buy(self, order_id, size, price):
         """
+        Place a sell limit order with given size and price.
+
+        Args:
+            order_id: the ID for the order to add
+            size: the size of the limit order to place
+            price: the price of the limit order to place
+
+        Returns:
+            None
+
+        """
+        LIB.limit_buy(self._book, order_id, size, price)
+
+    def limit(self, side, order_id, size, price):
+        """
+        Place a sell limit order with given size and price.
+
+        Args:
+            side: the side of the order to place
+            order_id: the ID for the order to add
+            size: the size of the limit order to place
+            price: the price of the limit order to place
+
+        Returns:
+            None
+
+        """
+        LIB.limit_buy(self._book, side, order_id, size, price)
 
     def has(self, order_id):
         """
+        Return true if the order with given ID is in the book, false otherwise.
+
+        Args:
+            order_id: the ID of the order to check for existence of
+
+        Returns:
+            True if the order is in the book, False otherwise
+
         """
+        return LIB.has(self._book, order_id)
 
     def get(self, order_id):
         """
+        Return a pointer to the order with given ID.
+
+        Args:
+            order_id: the ID of the order to get
+
+        Returns:
+            an order object wrapped around the pointer
+
         """
+        # pointer = LIB.get(self._book, order_id)
+        raise NotImplementedError
 
     def cancel(self, order_id):
         """
-        """
+        Cancel an order with given order ID.
 
-    def market_sell(self, size, arrival):
-        """
-        """
+        Args:
+            order_id: the ID of the order to cancel
 
-    def market_buy(self, size, arrival):
-        """
-        """
+        Returns:
+            None
 
-    def market(self, side, size, arrival):
         """
+        LIB.cancel(self._book, order_id)
+
+    def market_sell(self, order_id, size):
         """
+        Place a market sell order.
+
+        Args:
+            order_id: the ID for the order to add
+            size: the size of the market order to place
+
+        Returns:
+            None
+
+        """
+        LIB.market_sell(self._book, order_id, size)
+
+    def market_buy(self, order_id, size):
+        """
+        Place a market buy order.
+
+        Args:
+            order_id: the ID for the order to add
+            size: the size of the market order to place
+
+        Returns:
+            None
+
+        """
+        LIB.market_buy(self._book, order_id, size)
+
+    def market(self, side, order_id, size):
+        """
+        Place a market order.
+
+        Args:
+            side: the side of the order to place
+            order_id: the ID for the order to add
+            size: the size of the market order to place
+
+        Returns:
+            None
+
+        """
+        LIB.market(self._book, side, order_id, size)
 
     def best_sell(self):
         """Return the best sell price in the book."""
