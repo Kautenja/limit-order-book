9fc3f62690f528c7db214f540b9d539b9d4bc83e
Kautenja <kautencreations@gmail.com>
2019-12-17 01:47:49 -0600

    update ctypes

--- lob/lob.py
+++ lob/lob.py
@@ -1,27 +1,69 @@
-"""A CTypes interface to a Limit Order Book (LOB)."""
+"""The Limit Order Book (LOB)."""
 import ctypes
 import os
 import glob
 
 
-# the path to the directory this file is in
-_MODULE_PATH = os.path.dirname(__file__)
-# the pattern to find the C++ shared object library
-_SO_PATH = 'lib_lob*'
-# the absolute path to the C++ shared object library
-_LIB_PATH = os.path.join(_MODULE_PATH, _SO_PATH)
+LIB_PATH = os.path.join(os.path.dirname(__file__), 'lib_lob*')
 try:  # load the library from the shared object file
-    _LIB = ctypes.cdll.LoadLibrary(glob.glob(_LIB_PATH)[0])
+    LIB = ctypes.cdll.LoadLibrary(glob.glob(LIB_PATH)[0])
 except IndexError:
     raise OSError('missing static lib_lob*.so library!')
 
+Size = ctypes.c_uint32
+Price = ctypes.c_uint64
+Volume = ctypes.c_uint32
 
 # setup the argument and return types for Initialize
-_LIB.Initialize.argtypes = None
-_LIB.Initialize.restype = ctypes.c_void_p
+LIB.new_.argtypes = None
+LIB.new_.restype = ctypes.c_void_p
 # setup the argument and return types for Delete
-_LIB.Delete.argtypes = [ctypes.c_void_p]
-_LIB.Delete.restype = None
+LIB.delete_.argtypes = [ctypes.c_void_p]
+LIB.delete_.restype = None
+
+
+
+# setup the argument and return types for best_sell
+LIB.best_sell.argtypes = [ctypes.c_void_p]
+LIB.best_sell.restype = Price
+# setup the argument and return types for best_buy
+LIB.best_buy.argtypes = [ctypes.c_void_p]
+LIB.best_buy.restype = Price
+# setup the argument and return types for best
+LIB.best.argtypes = [ctypes.c_void_p, ctypes.c_bool]
+LIB.best.restype = Price
+
+# setup the argument and return types for volume_sell_price
+LIB.volume_sell_price.argtypes = [ctypes.c_void_p, Price]
+LIB.volume_sell_price.restype = Volume
+# setup the argument and return types for volume_sell
+LIB.volume_sell.argtypes = [ctypes.c_void_p]
+LIB.volume_sell.restype = Volume
+# setup the argument and return types for volume_buy_price
+LIB.volume_buy_price.argtypes = [ctypes.c_void_p, Price]
+LIB.volume_buy_price.restype = Volume
+# setup the argument and return types for volume_buy
+LIB.volume_buy.argtypes = [ctypes.c_void_p]
+LIB.volume_buy.restype = Volume
+# setup the argument and return types for volume_price
+LIB.volume_price.argtypes = [ctypes.c_void_p, Price]
+LIB.volume_price.restype = Volume
+# setup the argument and return types for volume
+LIB.volume.argtypes = [ctypes.c_void_p]
+LIB.volume.restype = Volume
+
+# setup the argument and return types for size_at
+LIB.size_at.argtypes = [ctypes.c_void_p, Price]
+LIB.size_at.restype = Size
+# setup the argument and return types for size_sell
+LIB.size_sell.argtypes = [ctypes.c_void_p]
+LIB.size_sell.restype = Size
+# setup the argument and return types for size_buy
+LIB.size_buy.argtypes = [ctypes.c_void_p]
+LIB.size_buy.restype = Size
+# setup the argument and return types for size
+LIB.size.argtypes = [ctypes.c_void_p]
+LIB.size.restype = Size
 
 
 class LimitOrderBook:
@@ -29,11 +71,11 @@ class LimitOrderBook:
 
     def __init__(self):
         """Initialize a new limit order book."""
-        self._book = _LIB.Initialize()
+        self._book = LIB.new_()
 
     def __del__(self):
         """Delete this limit order book."""
-        _LIB.Delete(self._book)
+        LIB.delete_(self._book)
 
     def limit_sell(self, size, price, arrival):
         """
