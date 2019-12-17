296cadd60faa81a7accb17749c2477283fc56913
Kautenja <kautencreations@gmail.com>
2019-12-17 01:28:22 -0600

    cleanup

--- src/lib_lob.cpp
+++ src/lib_lob.cpp
@@ -25,10 +25,14 @@ using namespace LOB;
 // definitions of functions for the Python interface to access
 extern "C" {
     /// Initialize a new limit order book and return a pointer to it
-    EXP LimitOrderBook* Initialize() { return new LimitOrderBook(); }
+    EXP LimitOrderBook* Initialize() {
+        return new LimitOrderBook();
+    }
 
     /// Delete the limit order book, i.e., purge it from memory
-    EXP void Delete(LimitOrderBook* book) { delete book; }
+    EXP void Delete(LimitOrderBook* book) {
+        delete book;
+    }
 
     // limit_sell
     ///
@@ -57,44 +61,70 @@ extern "C" {
     // market
     ///
 
-    // Price best_sell
-    ///
+    /// Return the best sell price.
+    EXP Price best_sell(LimitOrderBook* book) {
+        return book->best_sell();
+    }
 
-    // Price best_buy
-    ///
+    /// Return the best buy price.
+    EXP Price best_buy(LimitOrderBook* book) {
+        return book->best_buy();
+    }
 
-    // Price best
-    ///
+    /// Return the best price for the given side.
+    EXP Price best(LimitOrderBook* book, Side side) {
+        return book->best(side);
+    }
 
     /// Return the total volume for the sell side of the book.
-    EXP Volume volume_sell_price(LimitOrderBook* book, Price price) { return book->volume_sell(price); }
+    EXP Volume volume_sell_price(LimitOrderBook* book, Price price) {
+        return book->volume_sell(price);
+    }
 
     /// Return the total volume for the sell side of the book.
-    EXP Volume volume_sell(LimitOrderBook* book) { return book->volume_sell(); }
+    EXP Volume volume_sell(LimitOrderBook* book) {
+        return book->volume_sell();
+    }
 
     /// Return the total volume for the buy side of the book.
-    EXP Volume volume_buy_price(LimitOrderBook* book, Price price) { return book->volume_buy(price); }
+    EXP Volume volume_buy_price(LimitOrderBook* book, Price price) {
+        return book->volume_buy(price);
+    }
 
     /// Return the total volume for the buy side of the book.
-    EXP Volume volume_buy(LimitOrderBook* book) { return book->volume_buy(); }
+    EXP Volume volume_buy(LimitOrderBook* book) {
+        return book->volume_buy();
+    }
 
     /// Return the volume at the given limit price.
-    EXP Volume volume_price(LimitOrderBook* book, Price price) { return book->volume(price); }
+    EXP Volume volume_price(LimitOrderBook* book, Price price) {
+        return book->volume(price);
+    }
 
     /// Return the total volume for the book.
-    EXP Volume volume(LimitOrderBook* book) { return book->volume(); }
+    EXP Volume volume(LimitOrderBook* book) {
+        return book->volume();
+    }
 
     /// Return the size at the given limit price.
-    EXP Size size_at(LimitOrderBook* book, Price price) { return book->size_at(price); }
+    EXP Size size_at(LimitOrderBook* book, Price price) {
+        return book->size_at(price);
+    }
 
     /// Return the total size of the sell side of the book (number of orders).
-    EXP Size size_sell(LimitOrderBook* book) { return book->size_sell(); }
+    EXP Size size_sell(LimitOrderBook* book) {
+        return book->size_sell();
+    }
 
     /// Return the total size of the buy side of the book (number of orders).
-    EXP Size size_buy(LimitOrderBook* book) { return book->size_buy(); }
+    EXP Size size_buy(LimitOrderBook* book) {
+        return book->size_buy();
+    }
 
     /// Return the total size of the book (number of orders).
-    EXP Size size(LimitOrderBook* book) { return book->size(); }
+    EXP Size size(LimitOrderBook* book) {
+        return book->size();
+    }
 }
 
 // un-define the macro
